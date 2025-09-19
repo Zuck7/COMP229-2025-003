38d75bd30a7d3e5bd165f9b6e06318651f93990a
Zuhriddin <111017966+Zuck7@users.noreply.github.com>
2025-09-19 12:52:19 -0400

    Minor changes and image

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -1,17 +1,22 @@
+import { Link } from "react-router-dom";
+import image_logo from "../assets/ZUCK.png"
 
 function Layout(){
     return(
        <>
        <h1>My Portfolio</h1>
        <nav className="navbar">
-        <img src="{image-logo}" alt="logo" className="logo"/>
-        <link to="/" >Home</link>
-        <link to="/about" >About</link>
-        <link to="/projects" >Projects</link>
-        <link to="/Services" >Services</link>
+        <img src="{image_logo}" alt="logo" className="logo"/>
+        <Link to="/" >Home</Link>
+        <Link to="/about" >About</Link>
+        <Link to="/projects" >Projects</Link>
+        <Link to="/Services" >Services</Link>
+        <Link to="/contact" >Contact</Link>
        </nav>
+       <br />
+       <hr />
        </>
-    )
+    );
 }
 
 export default Layout;
--- client/src/components/ListComponent.jsx
+++ client/src/components/ListComponent.jsx
@@ -1,4 +1,4 @@
-
+import RowComponent from "./rowComponent";
 
 function ListComponent({items}){
     return(
