5663077d069895654413a0b02954d904ff21b122
Zuhriddin <111017966+Zuck7@users.noreply.github.com>
2025-09-21 15:43:57 -0400

    minor changes

--- client/src/components/projects.jsx
+++ client/src/components/projects.jsx
@@ -1,18 +1,18 @@
 import ListComponent from "./ListComponent";
 
-// const data = [
-//     {
-//         imagePath: bi_analyst,
-//         title: 'BI Analyst',
-//         text: "I offer BI analyst services for commercial businesses. It includes assisting in developing dashboards and reports for business and regulatory needs; supporting in automating manual reporting tasks using Excel, SQL and BI tools"
+const data = [
+    {
+        imagePath: pokedex,
+        title: 'Pokedex Project',
+        text: "I offer BI analyst services for commercial businesses. It includes assisting in developing dashboards and reports for business and regulatory needs; supporting in automating manual reporting tasks using Excel, SQL and BI tools"
         
-//     }, 
-//     {
-//         imagePath: ui_uxdesigner,
-//         title: 'UI/UX Designer',
-//         text: "I design intuitive digital experiences that users love. From wireframes to polished interfaces, I create websites and apps that look great and work seamlessly. Let's turn your ideas into user-friendly designs that drive results."
-//     },
-// ]
+    }, 
+    {
+        imagePath: ui_uxdesigner,
+        title: 'UI/UX Designer',
+        text: "I design intuitive digital experiences that users love. From wireframes to polished interfaces, I create websites and apps that look great and work seamlessly. Let's turn your ideas into user-friendly designs that drive results."
+    },
+]
 
 
 function Projects(){
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,8 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-// import Home from './components/home.jsx';
-import About from './components/about.jsx';
+
 
 
 createRoot(document.getElementById('root')).render(
