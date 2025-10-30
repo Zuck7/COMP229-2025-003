0655508c6e9d84408b8ecb88fc79a82fd261630d
Zuhriddin <111017966+Zuck7@users.noreply.github.com>
2025-10-30 19:57:15 -0400

    Minor changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,10 @@
-import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [
+    react(),
     tailwindcss(),
-    reactRouter(),
-    tsconfigPaths(),
   ],
 });
